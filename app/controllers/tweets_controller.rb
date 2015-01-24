50c86f03773a9659a796f1a9396e39b4dda9fd32
David Schmetterling <David@mdi-43320.bc.edu>
2015-01-24 14:25:10 -0500

    Created Flash Message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,16 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+	def create
+		@tweet = Tweet.create(tweet_params)
+		flash.now[:success] = "Tweet Created"
+		render 'new'
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
