8e28e6cfec3d56e5f5d5d46d617688e453857a1e
David Schmetterling <David@mdi-19.bc.edu>
2015-01-25 12:43:51 -0500

    list my tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,7 @@ class TweetsController < ApplicationController
 
 	def new
 		@tweet = Tweet.new
+		@tweets = current_user.tweets
 	end
 
 	def create
@@ -12,6 +13,8 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new(tweet_params)
 		@tweet.user = current_user
 		@tweet.save
+
+		@tweets = current_user.tweets
 		
 		flash.now[:success] = "Tweet Created"
 		render 'new'
